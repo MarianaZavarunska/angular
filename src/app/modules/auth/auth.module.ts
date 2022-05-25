96190afd7d50e1259d9c87d02497ffed250a1d22
MarianaZavarunska <89205808+MarianaZavarunska@users.noreply.github.com>
2022-05-25 20:58:21 +0300

    add validator

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import {ReactiveFormsModule} from "@angular/forms";
 
 import { LoginComponent, RegisterComponent } from './components';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthService } from './services';
-import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
--- src/app/modules/auth/components/register/register.component.ts
+++ src/app/modules/auth/components/register/register.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {
+    AbstractControl,
+    FormControl,
+    FormGroup,
+    ValidationErrors,
+    Validators
+} from '@angular/forms';
 
 import {AuthService} from "../../services";
 import {Router} from "@angular/router";
@@ -11,6 +17,7 @@ import {Router} from "@angular/router";
 })
 export class RegisterComponent implements OnInit {
  form: FormGroup;
+ formUserNameError: string;
 
   constructor(private authService: AuthService, private router: Router) {
       this._createForm();
@@ -21,18 +28,33 @@ export class RegisterComponent implements OnInit {
 
   _createForm():void{
     this.form = new FormGroup( {
-        username: new FormControl(null, [Validators.minLength(2),Validators.maxLength(20)]),
-        password: new FormControl(null, [Validators.minLength(2),Validators.maxLength(20)]),
-        confirmPassword: new FormControl(null, [Validators.minLength(2),Validators.maxLength(20)]),
-    });
+        username: new FormControl(null, [Validators.required, Validators.minLength(2),Validators.maxLength(20)]),
+        password: new FormControl(null, [Validators.required, Validators.minLength(2),Validators.maxLength(20)]),
+        confirmPassword: new FormControl(null, [Validators.required, Validators.minLength(2),Validators.maxLength(20)]),
+    }, [this._checkPassword]);
   }
 
   register(): void {
-      const rawValue = this.form.getRawValue();
-      delete rawValue.confirmPassword;
+      const formData = this.form.getRawValue();
+      delete formData.confirmPassword;
 
-        this.authService.register(rawValue).subscribe(() => {
-            this.router.navigate(['login'])
-        })
-    }
+
+        this.authService.register(formData).subscribe(
+            () => {
+
+                this.router.navigate(['api/auth/login'])
+            },
+            (e) =>  {
+                console.log(e)
+                this.formUserNameError = e.error.username[0];
+                },
+        )
+  }
+
+  _checkPassword(form: AbstractControl):ValidationErrors|null{
+      const password = form.get('password');
+      const confirmPassword = form.get('confirmPassword');
+      return confirmPassword?.value === password?.value ? null : {different: "Passwords don't match"};
+
+  }
 }
